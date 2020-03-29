9db4e4fa0a46a5feb937333c4e80c98cafa9d9eb
Donovan Maidens <donovan@anomalous.co.za>
2020-03-29 15:17:34 +0200

    Add Editor Config

--- wpza-maintenance-development-mode.php
+++ wpza-maintenance-development-mode.php
@@ -14,7 +14,7 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Maintenance and Development Mode
- * Plugin URI:        https://wpza.co.za
+ * Plugin URI:        https://github.com/djm56/wpza-maintenance-development-mode
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
  * Author:            Donovan Maidens
